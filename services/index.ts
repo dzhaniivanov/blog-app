d0289d050ec10807124e6edee01b8d1311c0747c
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-08-01 18:28:39 +0300

    work on details query and apges

--- services/index.ts
+++ services/index.ts
@@ -37,6 +37,40 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query);
+  return result.postsConnection.edges;
+};
+
 export const getRecentPosts = async () => {
   const query = gql`
   query GetPostDetails(){
@@ -54,11 +88,11 @@ export const getRecentPosts = async () => {
   }
   `;
 
-  const result = await request(graphqlAPI, query);
-  return result.posts;
+  const result = await request(graphqlAPI, query, { slug });
+  return result.post;
 };
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
@@ -79,7 +113,7 @@ export const getSimilarPosts = async () => {
     }
   `;
 
-  const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query, { categories, slug });
   return result.posts;
 };
 
