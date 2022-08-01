b124a8edb10073adb3d44859f3d8e364579db53c
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-08-01 13:34:15 +0300

    work on postcard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,9 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { PostCard, Categories, PostWidget } from "../components";
+import { getPosts } from "../services";
 
-const posts = [
-  {
-    title: "React Testing",
-    excerpt: "Learn react testing",
-  },
-  {
-    title: "React with tailwind",
-    excerpt: "Learn react with tailwind",
-  },
-];
-
-const Home: NextPage = () => {
+const Home: NextPage = ({posts}) => {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -23,13 +13,13 @@ const Home: NextPage = () => {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
           <div className="lg:sticky relative top-8">
             <PostWidget />
-            <Categories/>
+            <Categories />
           </div>
         </div>
       </div>
@@ -38,3 +28,11 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+
+  return {
+    props: { posts },
+  };
+}
